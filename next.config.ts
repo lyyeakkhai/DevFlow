60fc44b6328767a236716dc588c2d605797f35bc
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2026-01-16 16:28:26 +0700

    Setup Image logo and all theme within globles.css

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+  experimental: {
+    // Enable filesystem caching for `next dev`
+    turbopackFileSystemCacheForDev: true,
+    // Enable filesystem caching for `next build`
+    turbopackFileSystemCacheForBuild: true,
+  },
 };
 
 export default nextConfig;
