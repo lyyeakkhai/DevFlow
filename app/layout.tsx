205e05ae1c3a6cad9fe40689e440471e79323b7b
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2026-01-18 18:43:34 +0700

    Setup Theme and navbar with ShadCN

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,21 @@ import "./globals.css";
 const inter = localFont({
   src: "./fonts/inter_VF.ttf",
   variable: "--font-inter",
-  weight: "100 200 300 400 500 600 700 800 900", 
+  weight: "100 200 300 400 500 600 700 800 900",
 });
 const spaceGrotesk = localFont({
   src: "./fonts/spaceGrotesk_VF.ttf",
   variable: "--font-space-grotesk",
-  weight: "300 400 500 600 700", 
+  weight: "300 400 500 600 700",
 });
 
 export const metadata: Metadata = {
-  title: "DevFlow project",
-  description: "A project management tool for developers.",
+  title: "DevFlow",
+  description:
+    "DevFlow is a collaborative developer platform where engineers ask questions, share knowledge, and solve real-world problems. Engage with AI-powered answers, collaborate in live coding meeting rooms with screen sharing, and learn modern development topics including Data Structures & Algorithms, Next.js full-stack development, backend systems, AI engineering, and scalable system design.",
+  icons: {
+    icon: "../public/images/site-logo.svg",
+  }
 };
 
 export default function RootLayout({
@@ -25,11 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${inter.className} ${spaceGrotesk.variable} antialiased `}
-      >
-        {children}
-      </body>
+      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>{children}</body>
     </html>
   );
 }
