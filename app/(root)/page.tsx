bdd6eb0bd8e8fa0b946c50aeb17de545e7261740
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2026-01-22 23:20:38 +0700

    Implement content in home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,29 @@
-import { signOut } from "@/auth";
+import Link from "next/link";
+
 import { Button } from "@/components/ui/button";
-import ROUTE from "@/constants/routes";
+import ROUTES from "@/constants/routes";
 
 const Home = async () => {
   return (
     <>
-      <h1 className="h1-bold">Welcome to DevFlow</h1>
-      <form
-        className="mt-30 px-25 py-5"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTE.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log Out</Button>
-      </form>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button
+          className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900"
+          asChild
+        >
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+      <section className="mt-11">LocalSearch</section>
+      HomeFilter
+      <div className="mt-10 flex w-full flex-col gap-6">
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+        <p>Question Card 1</p>
+      </div>
     </>
   );
 };
