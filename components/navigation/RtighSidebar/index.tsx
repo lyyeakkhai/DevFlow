b37da4cdcacae0c7a5f2a6c6de85802a66589b37
lyyeakkhai <yeakhai.ly@student.cadt.edu.kh>
2026-01-23 11:57:24 +0700

    Implement local searchbar component

--- components/navigation/RtighSidebar/index.tsx
+++ components/navigation/RtighSidebar/index.tsx
@@ -26,7 +26,7 @@ const RightSidebar = () => {
 
 
   return(
-  <section className="pt-custom-scrollbar background-light900_dark200 light-border sticky top-0 left-0 hidden h-screen flex-col p-6 pt-36 sm:flex sm:w-fit lg:w-[330px] ">
+  <section className="pt-36 custom-scrollbar background-light900_dark200 light-border sticky right-0 top-0 flex h-screen w-[350px] flex-col gap-6 overflow-y-auto border-l p-6 shadow-light-300 dark:shadow-none max-xl:hidden">
     <div className="flex flex-col">
         <h3 className="h3-bold text-dark100_light900">
             Hot Network
